88ef43cdb6743c22efc49f4818340b7855fde877
오준영 <ojun-yeong@ojun-yeong-ui-MacBookPro.local>
2020-12-08 10:11:21 +0900

    Add Recyclerview and ItemDecoration

--- app/src/main/java/com/example/kywandroid/MainActivity.kt
+++ app/src/main/java/com/example/kywandroid/MainActivity.kt
@@ -1,32 +1,62 @@
 package com.example.kywandroid
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color.red
+import android.graphics.Paint
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ObservableField
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kywandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel by lazy { ViewModel() }
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.viewModel = viewModel
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         Log.d(TAG, ">>>>>> start")
         runExample()
         Log.d(TAG, ">>>>>> end")
     }
 
     private fun runExample() {
+        val adapter = MainRecyclerViewAdapter()
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.recyclerView.addItemDecoration(DivideDecoration(this))
+        adapter.setDataAndRefresh(listOf("aa", "bb", "cc", "dd", "ee", "ff", "gg"))
     }
 
-    inner class ViewModel {
-        var recyclerView = ObservableField<RecyclerView>()
+    inner class DivideDecoration(context: Context) : RecyclerView.ItemDecoration() {
+        private val paint: Paint = Paint()
+        val strokeWidth = 2
+
+        init {
+            paint.strokeWidth = context.resources.displayMetrics.density * strokeWidth
+            paint.color = ContextCompat.getColor(context, R.color.red)
+        }
+
+        override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+            for (i in 0 until parent.childCount) {
+                val view = parent.getChildAt(i)
+                c.drawLine(
+                    view.left.toFloat(),
+                    view.bottom.toFloat(),
+                    view.right.toFloat(),
+                    view.bottom.toFloat(),
+                    paint
+                )
+
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/kywandroid/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/kywandroid/MainRecyclerViewAdapter.kt
@@ -1,22 +1,45 @@
 package com.example.kywandroid
 
-import android.view.View
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.get
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.kywandroid.databinding.ListItemBinding
 
 class MainRecyclerViewAdapter : RecyclerView.Adapter<MainRecyclerViewAdapter.ViewHolder>() {
     var list = listOf<String>()
+    lateinit var binding: ListItemBinding
 
     override fun getItemCount(): Int = list.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.list_item,
+            parent,
+            false
+        )
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = list.get(position)
+        holder.bind(item)
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+    fun setDataAndRefresh(datas: List<String>) {
+        list = datas
+        notifyDataSetChanged()
     }
 
+    inner class ViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: String) {
+            binding.titleTextView.text = "Main ${item}"
+            binding.contentTextView.text = "Sub ${item}"
+        }
+    }
 }
